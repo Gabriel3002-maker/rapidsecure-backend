5ef9eac728fbc69f252ffb1c6f397665b34b4c04
Gabriel Vargas <gv8536892@gmail.com>
2024-07-14 15:42:45 +0200

    fix in reporte-emergencia

--- src/main/java/com/rapidsecure/rapidsecure/config/SwaggerConfig.java
+++ src/main/java/com/rapidsecure/rapidsecure/config/SwaggerConfig.java
@@ -8,8 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
 
 @Configuration
 public class SwaggerConfig {
